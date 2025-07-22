7eb46c4e048c58ec3116d3f4795c90e71a9a763d
awchjimmy <awchjimmy@gmail.com>
2025-07-22 11:28:58 +0800

    filter by chapter

--- scriptures/admin.py
+++ scriptures/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from .models import Book, Verse
 
+class VerseAdmin(admin.ModelAdmin):
+    list_filter = ['chapter']
+
 # Register your models here.
 admin.site.register(Book)
-admin.site.register(Verse)
+admin.site.register(Verse, VerseAdmin)
