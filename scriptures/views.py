34ae696be1989879d67e618d1bb8e1fffc4bae17
awchjimmy <awchjimmy@gmail.com>
2025-07-24 08:59:37 +0800

    Update views.py

--- scriptures/views.py
+++ scriptures/views.py
@@ -3,20 +3,30 @@ from django.shortcuts import get_object_or_404, get_list_or_404, render, redirec
 from .models import Book, Verse
 
 # Create your views here.
+def load_sidebar():
+    context = {}
+    old_testament = Book.objects.filter(id__gte=1, id__lte=39)
+    new_testament = Book.objects.filter(id__gte=40, id__lte=66)
+    context['old_testament'] = old_testament
+    context['new_testament'] = new_testament
+
+    return context
+
 def index(request):
     return redirect('scripture:chapter', book=1, chapter=1)
 
 def chapter(request, book, chapter):
-    old_testament = Book.objects.filter(id__gte=1, id__lte=39)
-    new_testament = Book.objects.filter(id__gte=40, id__lte=66)
-    chapters = range(1, get_object_or_404(Book, id=1).chapter+1)
-    verse_list = Verse.objects.filter(book=1, chapter=chapter)
-
+    # load sidebar
     context = {}
-    context['old_testament'] = old_testament
-    context['new_testament'] = new_testament
+    context.update(load_sidebar())
+
+    # load chapters
+    chapters = range(1, get_object_or_404(Book, id=1).chapter+1)
     context['chapter'] = chapter
     context['chapters'] = chapters
+
+    # load verses
+    verse_list = Verse.objects.filter(book=1, chapter=chapter)
     context['verses'] = verse_list
 
     return render(request, 'scriptures/index.html', context)
