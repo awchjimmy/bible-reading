c4db29460d0df65869de73788577d98ae805c284
awchjimmy <awchjimmy@gmail.com>
2025-08-08 10:41:54 +0800

    achievements templates

--- achievements/views.py
+++ achievements/views.py
@@ -1,6 +1,10 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from .models import BibleCover
+
 # Create your views here.
 def index(request):
-    return HttpResponse("hello from achievements.")
+    context = {}
+    context['book_covers'] = BibleCover.objects.all()
+    return render(request, 'achievements/index.html', context)
